02232dd27175cd362220f224590d985849881904
Pemba Tmg <ybatmg20@gmail.com>
2024-01-28 01:19:21 -0800

    file update

---
+++ firstprogram.py
@@ -0,0 +1,21 @@
+user_input=input("Enter from 1 to 10.\t")
+# my_dict={
+#     'Pen':'pen is as substance with which we can write on anything',
+#     'desk':'it is a thing where we put things',
+#     'table':'it is a thing in which we can sit',
+# }
+# print(my_dict[user_input])
+
+my_list={
+    '1':'You are hero Alom',
+    '2':'You are Rajnikandh',
+    '3':'You are Bhupendra Jogi',
+    '4':'You are Amrish Puri',
+    '5':'You are Anmol KC',
+    '6':'You are Rekha Thapa',
+    '7':'You are Saroj Tamang',
+    '8':'You are Nikhil Upreti',
+    '9':'You are Nikisha Shrestha ',
+    '10':'You are Guras Nani',
+}
+print(my_list[user_input])
