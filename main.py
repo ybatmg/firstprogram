f39a303b791d525f1b491ba75f896f220d7fdda3
Pemba Tmg <ybatmg20@gmail.com>
2024-01-27 23:33:39 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#BASIC datatypes
+
+PI=3.14
+
+my_num=12
+my_float=13.4
+my_str="Hello"
+my_bool="true"
+
+
+list_of_num = [12,23,434,3434,343,4342,43]
+list_of_n = ['34','41','hellos','24']
+
+
+tuple_of_num = (12,23,43,54,53,12,11,14,23,42)
+
+print(PI)
+print(my_num)
+print(my_float)
+print(my_str)
+print(my_bool)
+print(list_of_num)
+print(list_of_n)
+
+mydict={
+    "name":"Pemba",
+    "age":23,
+    "address":"ktm"
+}
+
+myset={12,15,55,65,78,90}
